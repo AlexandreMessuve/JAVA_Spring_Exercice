c7f50882dc5b12b914bcfc1bd1587945a58a656f
Alexandre Messuve <alexandre.messuves@gmail.com>
2024-07-11 10:57:28 +0200

    ajout structure.

---
+++ rest/exercice03/src/main/java/com/example/exercice03/Exercice03Application.java
@@ -0,0 +1,13 @@
+package com.example.exercice03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercice03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercice03Application.class, args);
+	}
+
+}
