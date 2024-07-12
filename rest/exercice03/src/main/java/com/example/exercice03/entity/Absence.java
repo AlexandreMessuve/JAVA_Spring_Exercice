8d023befbe4a0354f97dc924fef7fb0d82b27689
alexandremessuve <alexandremessuve@nc-ass-vip.sdv.fr>
2024-07-12 09:58:19 +0200

    ajout entity Absence

---
+++ rest/exercice03/src/main/java/com/example/exercice03/entity/Absence.java
@@ -0,0 +1,25 @@
+package com.example.exercice03.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Absence {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Temporal(TemporalType.DATE)
+    private LocalDate date;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @Column(name = "employee_id")
+    private Employee employee;
+}
